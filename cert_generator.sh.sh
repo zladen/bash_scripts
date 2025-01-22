3e88d71c162316a850040ad8bd0de50031e19e2d
Pavel Andreev <zladen1983@gmail.com>
2025-01-22 15:14:33 +0300

    added error checking

--- cert_generator.sh.sh
+++ cert_generator.sh.sh
@@ -1,37 +1,43 @@
 #!/bin/bash
 
 # Генерация сертификата и ключа
+set -euo pipefail
 
-# Установка OpenSSL
-if ! command -v openssl &> /dev/null; then
-  sudo apt update && sudo apt install -y openssl
-  if [ $? -ne 0 ]; then
-    exit 1
-  fi
-fi
-
-# Генерация сертификата
+# Параметры
 CERT_DIR="/etc/ssl/self_signed_cert"
 CERT_NAME="self_signed"
 DAYS_VALID=3650
-mkdir -p "$CERT_DIR"
 CERT_PATH="$CERT_DIR/$CERT_NAME.crt"
 KEY_PATH="$CERT_DIR/$CERT_NAME.key"
+SUBJECT="/C=US/ST=State/L=City/O=Organization/OU=Department/CN=example.com"
 
-openssl req -x509 -nodes -days $DAYS_VALID -newkey rsa:2048 \
+# Установка OpenSSL
+if ! command -v openssl &> /dev/null; then
+  echo "OpenSSL не установлен. Устанавливаем..."
+  sudo apt update && sudo apt install -y openssl || { echo "Ошибка установки OpenSSL"; exit 1; }
+fi
+
+# Создание директории для сертификатов
+mkdir -p "$CERT_DIR"
+
+# Генерация сертификата
+echo "Генерация сертификата..."
+if openssl req -x509 -nodes -days "$DAYS_VALID" -newkey rsa:2048 \
   -keyout "$KEY_PATH" \
   -out "$CERT_PATH" \
-  -subj "/C=US/ST=State/L=City/O=Organization/OU=Department/CN=example.com"
-
-if [ $? -eq 0 ]; then
+  -subj "$SUBJECT"; then
+  echo "SSL-сертификат успешно сгенерирован."
   echo "SSL CERTIFICATE PATH: $CERT_PATH"
   echo "SSL KEY PATH: $KEY_PATH"
 else
+  echo "Ошибка генерации SSL-сертификата."
   exit 1
 fi
 
+# Информация для пользователя
 echo "============================================================"
-echo "Сертификат с генерирован"
-echo "Сертификат - /etc/ssl/self_signed_cert/self_signed.crt"
-echo "Приватный ключ сертификата - /etc/ssl/self_signed_cert/self_signed.key"
+echo "Сертификат сгенерирован:"
+echo "Сертификат: $CERT_PATH"
+echo "Приватный ключ: $KEY_PATH"
+
 
